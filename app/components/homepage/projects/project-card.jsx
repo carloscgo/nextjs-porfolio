10b8c3038ff397d0cf8c379dbed309562affa496
Carlos Camacho <carloscgo123@gmail.com>
2024-03-31 17:33:27 -0500

    feat: update information

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -34,7 +34,6 @@ function ProjectCard({ project }) {
             <span className="text-amber-300">{project.name}</span>
             <span className="text-gray-400">{`',`}</span>
           </div>
-
           <div className="ml-4 lg:ml-8 mr-2">
             <span className=" text-white">tools:</span>
             <span className="text-gray-400">{` ['`}</span>
@@ -57,10 +56,18 @@ function ProjectCard({ project }) {
             <span className="text-gray-400">,</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
-            <span className="text-white">Description:</span>
+            <span className="text-white">description:</span>
             <span className="text-cyan-400">{' ' + project.description}</span>
             <span className="text-gray-400">,</span>
           </div>
+          {project.demo && (
+            <div>
+              <span className="ml-4 lg:ml-8 mr-2 text-white">demo:</span>
+              <span className="text-gray-400">{`'`}</span>
+              <span className="text-amber-300"><a href={demo} target='blank'>{project.demo}</a>{project.demo}</span>
+              <span className="text-gray-400">{`',`}</span>
+            </div>
+          )}
           <div><span className="text-gray-400">{`};`}</span></div>
         </code>
       </div>
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,46 @@
+
+
 export const experiences = [
   {
-    id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    id: 14,
+    title: 'Senior Software Developer',
+    company: "Personal Soft, SAS - Colombia",
+    duration: "(Jul 2023 - Present)"
   },
   {
-    id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    id: 15,
+    title: 'Full Stack Developer',
+    company: "Empresa Inteligente Asociados - Mexico",
+    duration: "(Dic 2022 - Jul 2023)"
   },
   {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    id: 16,
+    title: 'Full Stack Developer',
+    company: "Mercury Cash - USA",
+    duration: "(Feb 2018 - Oct 2022)"
+  },
+  {
+    id: 17,
+    title: 'Full Stack Developer',
+    company: "Empresa Inteligente Asociados - Mexico (freelance)",
+    duration: "(Oct 2017 - Feb 2018)"
+  },
+  {
+    id: 18,
+    title: 'Full Stack Developer',
+    company: "Grupo Adames, Drink2Go - Rep Dominicana (freelance)",
+    duration: "(Mar 2017 - Feb 2018)"
+  },
+  {
+    id: 19,
+    title: "Development Director, Web Programmer",
+    company: "Net Sistem C.A. - Venezuela",
+    duration: "(Oct 2029 - Mar 2017)"
+  },
+  {
+    id: 20,
+    title: "Head of Development and Programming",
+    company: "DHD Soluciones C.A. - Venezuela",
+    duration: "(Feb 2006 - Oct 2009)"
   }
 ]
